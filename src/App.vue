885d932c59559b4a5f0e7d429aec6b2395bd4000
Ivan Egorov <www.Len244@gmail.com>
2021-07-23 23:59:01 +0300

    Commiting before my two week trip

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,105 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <to-do-list v-bind:tasks="this.tasks" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ToDoList from "./components/ToDoList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ToDoList,
+  },
+  data() {
+    return {
+      db: false,
+      tasks: {},
+    };
+  },
+  beforeCreate() {
+    let databaseRequest = window.indexedDB.open("pwaTodo", 3);
+
+    const mockupToDo = [
+      {
+        text: "Wash the dishes",
+        status: false,
+        subtasks: [
+          { text: "Wash plastes", status: false },
+          { text: "Wash kaddle", status: false },
+        ],
+      },
+      {
+        text: "Create ToDo app",
+        status: false,
+        subtasks: [
+          { text: "Connect local db", status: true },
+          { text: "Create mockup data", status: true },
+          { text: "Populate db with date", status: false },
+          { text: "Show data from db", status: false },
+          { text: "Add interaction", status: false },
+          { text: "Add styles", status: false },
+        ],
+      },
+    ];
+
+    databaseRequest.onerror = (event) => {
+      console.error("Database didn't opened. " + event.target.errorCode);
+    };
+
+    databaseRequest.onsuccess = (event) => {
+      console.log("Database connected");
+      this.db = event.target.result;
+
+      // It will work because onupgradeneeded goes before onsuccess
+      let testTrans = this.db.transaction(["tasks"]);
+      let tasksObjectStore = testTrans.objectStore("tasks");
+
+      let allData = tasksObjectStore.getAll();
+
+      allData.onsuccess = () => {
+        console.log(allData.result);
+        this.tasks = allData.result;
+      };
+    };
+
+    // COMPLETED: Check if old data are whiped with this method.
+    //            No, it's not get whiped if you not specify so
+    // TODO: Change objectStore delete to simple migration of all previous data smh if it is possible.
+    databaseRequest.onupgradeneeded = (event) => {
+      console.log("Need upgrade. Upgrading");
+      this.db = event.target.result;
+
+      let objectStore;
+
+      try {
+        objectStore = this.db.createObjectStore("tasks", {
+          autoIncrement: true,
+        });
+      } catch (DOMException) {
+        this.db.deleteObjectStore("tasks");
+        objectStore = this.db.createObjectStore("tasks", {
+          autoIncrement: true,
+        });
+      }
+
+      mockupToDo.forEach((task) => {
+        objectStore.add(task);
+      });
+    };
   },
 };
 </script>
 
 <style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="toDoList">
+    <div class="task-container" v-for="task in tasks" v-bind:key="task.text">
+      <div class="current-task">
+        <p>{{ task.text }}</p>
+        <input type="checkbox" name="" id="" v-bind:checked="task.status" />
+      </div>
+      <div
+        class="subtasks"
+        v-for="subtask in task.subtasks"
+        v-bind:key="subtask.text"
+      >
+        <div class="subtask">
+          <p>{{ subtask.text }}</p>
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            v-bind:checked="subtask.status"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoList",
+  props: {
+    tasks: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.task-container {
+  background-color: #787878;
+}
+
+.current-task {
+  display: flex;
+  gap: 16px;
+  align-items: center;
+  justify-content: center;
+  background-color: red;
+}
+
+.subtask {
+  display: flex;
+  gap: 16px;
+  align-items: center;
+  justify-content: center;
+  background-color: green;
+}
+</style>
