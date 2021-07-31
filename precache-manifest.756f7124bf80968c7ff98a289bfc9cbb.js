cee81028f2d08c61f12fadc494646c087e778898
Ivan Egorov <www.Len244@gmail.com>
2021-07-31 14:52:11 +0300

    gh-pages

---
+++ precache-manifest.756f7124bf80968c7ff98a289bfc9cbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f475fc4f3490d12394b",
+    "url": "/pwa-todo/css/app.4ae4f3f9.css"
+  },
+  {
+    "revision": "6808b2597a354ec22c856f9b2977324f",
+    "url": "/pwa-todo/img/delete_forever_black_24dp.6808b259.svg"
+  },
+  {
+    "revision": "65ff7626dab3e066c0a1814a44aa4a2c",
+    "url": "/pwa-todo/img/done_black_24dp.65ff7626.svg"
+  },
+  {
+    "revision": "4e7182f255041edfa1112c8e670397f3",
+    "url": "/pwa-todo/index.html"
+  },
+  {
+    "revision": "6f475fc4f3490d12394b",
+    "url": "/pwa-todo/js/app.fd62d5a2.js"
+  },
+  {
+    "revision": "ea9fca1d854257d5dddb",
+    "url": "/pwa-todo/js/chunk-vendors.dc9fb6ff.js"
+  },
+  {
+    "revision": "5a5d8f90014a5b6908090fa7c8ff3cb0",
+    "url": "/pwa-todo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-todo/robots.txt"
+  }
+]);
